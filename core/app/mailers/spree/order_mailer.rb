b9d922820b2bbfe04b9001aff0e7e711d6f952dc
Shelvetco <shel@vetco.net>
2015-12-22 16:57:54 -0800

    Make OrderMailer DRY

--- core/app/mailers/spree/order_mailer.rb
+++ core/app/mailers/spree/order_mailer.rb
@@ -1,16 +1,19 @@
 module Spree
   class OrderMailer < BaseMailer
-    def confirm_email(order, resend = false)
-      @order = order.respond_to?(:id) ? order : Spree::Order.find(order)
-      subject = (resend ? "[#{Spree.t(:resend).upcase}] " : '')
-      subject += "#{Spree::Store.current.name} #{Spree.t('order_mailer.confirm_email.subject')} ##{@order.number}"
-      mail(to: @order.email, from: from_address, subject: subject)
+    def confirm_email(*args)
+      confirm_cancel_email(true,*args)
     end
 
-    def cancel_email(order, resend = false)
+    def cancel_email(*args)
+      confirm_cancel_email(false,*args)
+    end
+    
+    private
+    
+    def confirm_cancel_email(is_confirm, order, resend = false)
       @order = order.respond_to?(:id) ? order : Spree::Order.find(order)
       subject = (resend ? "[#{Spree.t(:resend).upcase}] " : '')
-      subject += "#{Spree::Store.current.name} #{Spree.t('order_mailer.cancel_email.subject')} ##{@order.number}"
+      subject += "#{Spree::Store.current.name} #{Spree.t("order_mailer.#{is_confirm ? "confirm" : "cancel")}_email.subject")} ##{@order.number}"
       mail(to: @order.email, from: from_address, subject: subject)
     end
   end
